a045c16a8f227c9bf5d002b786b1b7dbd8d5e76c
Alcides <tu_correo@ejemplo.com>
2026-09-23 00:01:14 -0500

    Actualizar dedicatoria en flower.html

--- anim.js
+++ anim.js
@@ -65,7 +65,7 @@ setInterval(updateLyrics, 500);
 function ocultarTitulo() {
   var titulo = document.querySelector(".titulo");
   if (titulo) {
-    titulo.style.animation = "fadeOut 3s ease-in-out forwards";
+    titulo.style.animation = "fadeOut 5s ease-in-out forwards";
     setTimeout(function () {
       titulo.style.display = "none";
     }, 3000);
